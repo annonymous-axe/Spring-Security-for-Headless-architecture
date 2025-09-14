032e105c588d8a6ecf02bdb43acc67297cd44fa0
kamlu <ksb17219@gmail.com>
2025-09-14 16:11:26 +0530

    Created project for Spring Security with essential dependencies.

---
+++ src/main/java/demo/security/headless/HeadlessArchitectureSecurityApplication.java
@@ -0,0 +1,13 @@
+package demo.security.headless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessArchitectureSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlessArchitectureSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/security/headless/HeadlessArchitectureSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.security.headless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadlessArchitectureSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
